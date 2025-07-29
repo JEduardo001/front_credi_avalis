89204655cd63a70bf53369627030bce459a98735
Jose eduardo quirarte Arce <eduardoarce773@gmail.com>
2025-07-29 10:30:50 -0700

    All Design Screens

---
+++ controller/protected/home.js
@@ -0,0 +1,3 @@
+ // Aquí puedes reemplazar "Juan Pérez" por el nombre real desde backend o localStorage
+ const nombre = "Juan Pérez";
+ document.getElementById("nombreUsuario").textContent = nombre;
